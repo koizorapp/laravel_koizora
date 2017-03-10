e4c1bd581c984fefe415c9a13a8bc5be928e1782
liulei <koizora_pp@163.com>
2017-03-10 21:44:26 +0800

    通过php artisan make:model Question -m 创建了 Question.php Model 和 Question的 migration; 在Question的migrate中创建了questions表，执行了 phpartisan migrate

--- database/migrations/2017_03_07_171721_create_questions_table.php
+++ database/migrations/2017_03_07_171721_create_questions_table.php
@@ -15,6 +15,14 @@ class CreateQuestionsTable extends Migration
     {
         Schema::create('questions', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('title')->comment('标题');
+            $table->text('body')->comment('内容');
+            $table->integer('user_id')->comment('用户ID');
+            $table->integer('comments_count')->default(0)->comment('评论数量');
+            $table->integer('followers_count')->default(1)->comment('关注数量');
+            $table->integer('answers_count')->default(0)->comment('回答数量');
+            $table->string('close_comment',8)->default('F')->comment('是否关闭评论,F没有关闭,T关闭');
+            $table->string('is_hidden',8)->default('F')->comment('是否隐藏,F不是隐藏,T隐藏');
             $table->timestamps();
         });
     }
